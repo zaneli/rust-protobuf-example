46952aedb8f9fa0cbd337fcb83a72fc2a838ad5e
zaneli <shun.otani@gmail.com>
2016-12-17 22:21:16 +0900

    Use io::Error instead of Box<Error> (thanks to @KeenS #1)

--- src/main.rs
+++ src/main.rs
@@ -6,40 +6,42 @@ mod list_people;
 
 use protobuf::error::ProtobufError;
 use std::{env, process};
-use std::error::Error;
-use std::io::{stderr, Write};
+use std::io::{self, stderr, Write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     get_module_name(&args)
-        .and_then(|f| get_file_path(&args).and_then(|p| f(p).map_err(From::from)))
+        .map_err(ProtobufError::IoError)
+        .and_then(|f| get_file_path(&args).map_err(ProtobufError::IoError).and_then(f))
         .unwrap_or_else(|e| {
             stderr().write_fmt(format_args!("{}\n", e)).unwrap();
             process::exit(-1);
         })
 }
 
-fn get_module_name(args: &Vec<String>)
-                   -> Result<fn(&str) -> Result<(), ProtobufError>, Box<Error>> {
+fn get_module_name(args: &Vec<String>) -> Result<fn(&str) -> Result<(), ProtobufError>, io::Error> {
     if args.len() >= 2 {
         match args[1].as_ref() {
             "add_person" => Ok(add_person::execute),
             "list_people" => Ok(list_people::execute),
             other => {
-                Err(From::from(format!("Unexpected module name: {}. (expected 'add_person' or \
-                                        'list_people')",
-                                       other)))
+                Err(io::Error::new(io::ErrorKind::NotFound,
+                                   (format!("Unexpected module name: {}. (expected \
+                                             'add_person' or 'list_people')",
+                                            other))))
             }
         }
     } else {
-        Err(From::from("Usage: cargo run <module_name> <file_path>"))
+        Err(io::Error::new(io::ErrorKind::Other,
+                           "Usage: cargo run <module_name> <file_path>".to_string()))
     }
 }
 
-fn get_file_path(args: &Vec<String>) -> Result<&str, Box<Error>> {
+fn get_file_path(args: &Vec<String>) -> Result<&str, io::Error> {
     if args.len() >= 3 {
         Ok(&args[2])
     } else {
-        Err(From::from("Usage: cargo run <module_name> <file_path>"))
+        Err(io::Error::new(io::ErrorKind::Other,
+                           "Usage: cargo run <module_name> <file_path>".to_string()))
     }
 }
